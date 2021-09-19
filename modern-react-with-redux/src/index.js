9cc23ad3968b2a14ed71d7f5d9dc8ed9071d83c0
NGOC DUONG <apple@Ngocs-MacBook-Pro.local>
2021-09-19 16:59:58 +0700

    feat: add index file

--- modern-react-with-redux/src/index.js
+++ modern-react-with-redux/src/index.js
@@ -2,20 +2,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function getButtonText() {
-    return 'Click on me!'
-}
-
 // Create a react component
 const App = () => {
+    const buttonText = { text: 'Click me' }
     return (
         <div>
-            <label className="label" for="name">
+            <label className="label" htmlFor="name">
                 Enter name:
             </label>
             <input id="name" type="text"/>
             <button style={{ backgroundColor:'blue', color:'white' }}>
-                {getButtonText()}
+                {buttonText.text}
             </button>
         </div>
     );
