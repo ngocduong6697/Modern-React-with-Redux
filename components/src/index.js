8193a058048584b9cf48dd117f9c9a935359af76
NGOC DUONG <apple@Ngocs-MacBook-Pro.local>
2021-09-20 23:38:23 +0700

    feat: implement approval card and comment detail

--- components/src/index.js
+++ components/src/index.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import CommentDetail from "./components/CommentDetail";
 import faker from 'faker';
+import ApprovalCard from "./components/ApprovalCard";
 
 const App = () => {
-    return (
-        <div class="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">Ngoc</a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">Nice blog post</div>
-                </div>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="ui container comments">
+      <ApprovalCard />
+      <CommentDetail
+        author="Ngoc"
+        timeAgo="Today At 10:00PM"
+        content="This block is nice"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Duong"
+        timeAgo="Today At 9:00PM"
+        content="I love this block"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Van"
+        timeAgo="Today At 11:00PM"
+        content="I'm not sure about that!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Toan"
+        timeAgo="Today At 5:00PM"
+        content="Do you think is good?"
+        avatar={faker.image.avatar()}
+      />
+    </div>
+  );
+};
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
