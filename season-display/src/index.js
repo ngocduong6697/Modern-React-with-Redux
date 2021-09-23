eb595062c334c46e42ae71cefa4169c900931b42
NGOC DUONG <apple@Ngocs-MacBook-Pro.local>
2021-09-23 22:17:05 +0700

    feat: init project season display

---
+++ season-display/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div>Hello Ngocz1</div>
+    )
+}
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector( '#root')
+)
